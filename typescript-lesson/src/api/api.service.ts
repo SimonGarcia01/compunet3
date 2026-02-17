44984c502bbfde015d09eded31612f1e1a8f15e0
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2026-02-17 18:16:58 -0500

    Added changes for SOLID principle explenation.

--- typescript-lesson/src/api/api.service.ts
+++ typescript-lesson/src/api/api.service.ts
@@ -1,13 +1,57 @@
 import axios from "axios";
-import type { PokeAPI } from "../interfaces/pokeapi";
 
-export class ApiService {
-    async getPokemon(name:string){
+//Strategy pattern here using interface
+//Now all services must implement it
+export interface HttpAdapter {
+    getPokemon<T>(name:string): Promise<T>
+}
+
+//Now lets make a service that doesn't depend on the API
+export class ApiServiceAxios implements HttpAdapter {
+    //Now it doesn't expect the specific structure of the API
+    async getPokemon<T>(name:string): Promise<T> {
         const url= `https://pokeapi.co/api/v2/pokemon/${name}`;
-        //Method is not a promise, but axios will return a promise
-        //A promise returns a result as long as you wait for it
-        //Now we can use the interface
-        const pokemon = await axios.get<PokeAPI>(url);
-        return pokemon;
+
+        //Now get the data, but at this point you don't know the structure of the data = Generic T
+        const {data} = await axios.get<T>(url);
+
+        return data;
     }
 }
+
+export class ApiServiceFetch implements HttpAdapter {
+    async getPokemon<T>(name:string): Promise<T> {
+        const url= `https://pokeapi.co/api/v2/pokemon/${name}`;
+        const response = await fetch(url);
+        const data: T = await response.json();
+        return data;
+    }
+}
+
+// export class ApiServiceAxios {
+//     async getPokemon(name:string){
+//         const url= `https://pokeapi.co/api/v2/pokemon/${name}`;
+//         //Method is not a promise, but axios will return a promise
+//         //A promise returns a result as long as you wait for it
+//         //Now we can use the interface
+//         //data is returned by axios, and it holds the structure of PokeAPI
+//         const {data} = await axios.get<PokeAPI>(url);
+//         //This is desctructuring the pokemon, now we only get the data
+//         //Desctructuring can have any values you want, but they must be in the same order as the original object
+//         return data;
+//     }
+// }
+
+// export class ApiServiceFetch {
+//     async getPokemon(name:string): Promise<PokeAPI> {
+//         const url= `https://pokeapi.co/api/v2/pokemon/${name}`;
+//         //Get the info
+//         const response = await fetch(url);
+
+//         //Give structure to the data
+//         const data: PokeAPI = await response.json();
+        
+//         //return the data with the structure
+//         return data;
+//     }
+// }
--- typescript-lesson/src/class-basics/classes.ts
+++ typescript-lesson/src/class-basics/classes.ts
@@ -1,5 +1,4 @@
-import { ApiService } from "../api/api.service";
-
+import { ApiServiceAxios, ApiServiceFetch, type HttpAdapter } from "../api/api.service";
 
 class Student {
     id: number;
@@ -9,14 +8,14 @@ class Student {
 
     //You now make the constructor
     //A private readonly property is a property that can only be set in the constructor and cannot be changed afterwards
-    constructor(id: number, name: string, age: number, isActive: boolean, private readonly apiService: ApiService) {
+    //Now we inject the api interface, so we can use any class
+    constructor(id: number, name: string, age: number, isActive: boolean, private readonly httpAdapter: HttpAdapter) {
         //This refers to the instance of the class
         //If the class had a parent class, you use super.X
         this.id = id;
         this.name = name;
         this.age = age;
         this.isActive = isActive;
-        this.apiService = apiService;
     }
 
     //Method with return
@@ -29,14 +28,20 @@ class Student {
         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 
-    async getPokemon(name:string){
-        return await this.apiService.getPokemon(name);
+    //Now make the method generic
+    async getPokemon<T>(name:string): Promise<T> {
+        return await this.httpAdapter.getPokemon<T>(name);
     }
 }
 
 //Now you must add the api service to the constructor
-export const student1 = new Student(1, "student1", 34, true, new ApiService());
+
+//The constructor can be using any service now
+const serviceAxios = new ApiServiceAxios();
+const serviceFetch = new ApiServiceFetch();
+
+export const student1 = new Student(1, "student1", 34, true, serviceAxios);
 
 //Here we make the petition to the API
 const pokemon = await student1.getPokemon("bulbasaur");
-console.log(pokemon.data.name);
+console.log(pokemon);
