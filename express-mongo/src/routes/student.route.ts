0ce8bc5b8e26acc5505f378ff1a2fb19e250f143
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2026-03-05 15:00:00 -0500

    Finished example, mostly.

--- express-mongo/src/routes/student.route.ts
+++ express-mongo/src/routes/student.route.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { studentController } from "../controllers/student.controller";
+import { CreateStudentDto } from "../dtos/student.dto";
+import { validateDto } from "../middlewares/validate-dto";
+
 
 export const studentRouter: Router = Router();
 
 
 //Routes for other CRUD Operations
 //Create a new student
-studentRouter.post("/", studentController.create);
+studentRouter.post("/", validateDto(CreateStudentDto),  studentController.create);
 
 //Routing to get all students
 studentRouter.get("/", studentController.getAll);
@@ -14,7 +17,7 @@ studentRouter.get("/", studentController.getAll);
 //Get one student by id
 studentRouter.get("/:id", studentController.getOne);
 
-// //Update a student
-// studentRouter.put("/:id", studentController.update);
+// //Update part of a student
+// studentRouter.patch("/:id",validateDto(UpdateStudentDto), studentController.update);
 // //Delete a student
 // studentRouter.delete("/:id", studentController.delete);
