f9595c5fe62bda0a13b608baf9ab602c5f51755b
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2026-05-19 21:19:30 -0500

    Fixing basic project

--- graphql-basics/src/app.module.ts
+++ graphql-basics/src/app.module.ts
@@ -19,7 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 			host: process.env.DB_HOST,
 			port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
 			database: process.env.DB_NAME,
-			username: process.env.DB_USER,
+			username: process.env.DB_USERNAME,
 			password: process.env.DB_PASSWORD,
 			autoLoadEntities: true,
 			synchronize: true //Only use this in development, not in production
